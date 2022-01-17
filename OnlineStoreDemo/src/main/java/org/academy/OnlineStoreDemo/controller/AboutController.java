eb2c0e843622d90ac12ca4173c56863524cc19a5
IgorKlimovich <85802281+IgorKlimovich@users.noreply.github.com>
2022-01-17 21:23:21 +0300

    commit1

--- OnlineStoreDemo/src/main/java/org/academy/OnlineStoreDemo/controller/AboutController.java
+++ OnlineStoreDemo/src/main/java/org/academy/OnlineStoreDemo/controller/AboutController.java
@@ -1,15 +1,31 @@
 package org.academy.OnlineStoreDemo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.academy.OnlineStoreDemo.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.security.Principal;
+
+@Slf4j
 @Controller
 @RequestMapping("/about")
 public class AboutController {
 
+
+    private final UserService userService;
+
+    public AboutController( UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String getAboutPage(){
+    public String getAboutPage(Principal principal, Model model){
+        if (principal!=null){
+        model.addAttribute("userProf" , userService.findByLogin(principal.getName()));}
+        log.info("in get about page: return user to about page");
         return "about";
     }
 }
